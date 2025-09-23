9bfc48821de26fccd469cba97e943de10fca1150
carljoshua123 <cjgbernardo21@gmail.com>
2025-09-24 00:19:31 +0800

    twddwdw it there will be a content that will apperead.

--- 05-modal/starter/script.js
+++ 05-modal/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+// Select the modal element
+const modalEl = document.querySelector('.modal');
+
+// Select the overlay element
+const overlayEl = document.querySelector('.overlay');
+
+// Select the close button
+const btnCloseModalEl = document.querySelector('.close-modal');
+
+// Select ALL the open buttons using querySelectorAll
+const btnsOpenModalEl = document.querySelectorAll('.show-modal');
+
+// Verify our selections worked correctly
+console.log('Open buttons:', btnsOpenModalEl.length);
+
+
+const openModal = function () {
+  modalEl.classList.remove('hidden');
+  overlayEl.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modalEl.classList.add('hidden');
+  overlayEl.classList.add('hidden');
+};
+
+// Event listeners
+btnsOpenModalEl.forEach(btn => btn.addEventListener('click', openModal));
+btnCloseModalEl.addEventListener('click', closeModal);
+overlayEl.addEventListener('click', closeModal);
